c0ebba5b7903fdfb17fb6d9450e4f8fd5156a6bf
cdann <cdann@lintmachine.com>
2014-04-22 17:23:09 -0700

    Initial commit. Views mostly laid out.

---
+++ ACSMToolbox/ACSMToolbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ACSMToolbox
+//
+//  Created by cdann on 3/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ACSMToolbox/ACSMToolbox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ACSMToolbox
+//
+//  Created by cdann on 3/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
